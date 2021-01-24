b87eee115d38978d4b4ee9f0e7fbcce673fa23b2
Akootchook <77140613+Akootchook@users.noreply.github.com>
2021-01-23 23:38:50 -1000

    Added Part 7 for assignment
    I used the hex value of blue as the integer which is 0, 0, 255
    
    Assignment for Class 4 Thursday Jan 19, 2021

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -1,38 +1,70 @@
 import UIKit
 
-var str = "Hello, playground"
+//var str = "Hello, playground"
 
 //declare a variable, name it, myfirstvariable and type it as a String
-var myFirstVariable: String
+//var myFirstVariable: String
 //String is a data type that is represented by a series of characters. The string must be surrounded by ""
 
 //assign myFirstVariable to a string
-myFirstVariable = "I`ve created my first string"
+//myFirstVariable = "I`ve created my first string"
 
 //String Concatenation - combining strings together
 
 //declare and assign a second variable all in one line
-var mySecondVariable = "and look I combine 2 strings together"
+//var mySecondVariable = "and look I combine 2 strings together"
 
 //create a thrid variable to hold the string concatenation
 
-var myThirdVariable = myFirstVariable + mySecondVariable
+//var myThirdVariable = myFirstVariable + mySecondVariable
 
-print(myThirdVariable)
+//print(myThirdVariable)
 
 //decalare a variable, named helloMyNameIs. The string must be surrounded by ""
-var helloMyNameIs: String
+//var helloMyNameIs: String
 
-helloMyNameIs = "This is my homework"
+//helloMyNameIs = "This is my homework"
     
-var helloMyNameIsWhat = "this is a line from a track"
+//var helloMyNameIsWhat = "this is a line from a track"
 
-var helloMyNameIsWho = "Slim Shady lol. "
+//var helloMyNameIsWho = "Slim Shady lol. "
 //note sure why that popped in my head but I think I at least got an output
-var helloMyName = helloMyNameIsWho + helloMyNameIsWhat
+//var helloMyName = helloMyNameIsWho + helloMyNameIsWhat
 
-print(helloMyName)
+//print(helloMyName)
 
+// Part 8
 
+//Assignment #2 : Introduction to True and False Class 4 Thursday 1.21.21
 
+//var appState = false
 
+//print() - print function - it prints into the console. all functions have a name and followed by parenthesis ()
+//print(appState)
+
+//decalre and assigned two variables of type int
+//intergers are always whole numbers ? once there is a decimal is changes the value.
+//var numberOne = 5
+//var numberTwo = 7
+//var comparison = numberOne < numberTwo
+
+//var comparison = numberOne > numberTwo
+//print(comparison)
+
+//Part 7 assignment "Equal to" Operator. google operator is ==
+
+//var numberThree = 10
+
+//var number3 = 10
+
+//var comparison = numberThree == number3
+
+//print(comparison)
+
+var colorOfPen = 00255
+
+var isBlue = 00255
+
+var comparison = colorOfPen == isBlue
+//quote from the movie Liar Liar lol
+print(comparison)
